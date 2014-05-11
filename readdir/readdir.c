f021ea631cd420941069c190f104e92738ea10a1
Stéphane Kimmel <stephane.kimmel@student.uclouvain.be>
2014-05-12 00:59:43 +0200

    Readdir finie mais incompréhension des résultats

--- readdir/readdir.c
+++ readdir/readdir.c
@@ -4,8 +4,6 @@
 *  Le but de ce benchmark est de voir l'efficacité de readdir
 *  Et de créer un graphique affichable en html.
 *
-*  ATTENTION COMPRENDRE LA COMPILATION !!! 
-*  ATTENTION COMPRENDRE DANS QUEL CAS C EST PLUS LONG !!!
 **/
 
 #include <stdio.h>
@@ -20,62 +18,73 @@
 #include <errno.h>  
 
 #include "benchmark.h"
+#include "copy.h"
 
-void rand_str(char *dest, int length) {
-    char charset[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    while (length-- > 0) {
-        size_t index = (double) rand() / RAND_MAX * (sizeof charset - 1);
-        *dest++ = charset[index];
-    }
-    *dest = '\0';
+
+void chaineCaractere(char* file, int longueur)
+{
+	  char charset[] = "0123456789"
+                     "abcdefghijklmnopqrstuvwxyz"
+                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      while (longueur-- > 0 )
+      {
+      	size_t index = longueur%(sizeof file - 1);
+        *file++ = charset[index];
+      }
+      *file = '\0';
+
 }
 
-
 int main(int argc, char *argv[]){
+
+		// !!! Variables à changer
+	int longFichierAlloue = 10000;
+	char* nameDossier = "repertoire";
+	int nombreDeFichierMaxCree = 50000;
+
 	//création du répertoire :
-	if(mkdir("./repertoire",S_IRWXU)){
+	if(mkdir("repertoire",S_IRWXU)){ // ! j'ai changé de './repertoire'
 		perror("rmdir");
 	}
-	//création des variable de calcul benchmark:
+
+	//création et allocation des structures qui permettent de calculer les temps du benchmark:
 	timer *t = timer_alloc();
 	recorder *record = recorder_alloc("readdir.csv");
 	recorder *record2 = recorder_alloc("readdirfull.csv");
 
-	// !!! Variables à changer
-	int longueurChaineGene = 10000;
-	char* cheminDuDossier = "repertoire";
-	int nombreDeFichierMaxCree = 10000;
-
 	//ouvrir un document 
 	DIR *rep = NULL;
-	rep = opendir(cheminDuDossier);
+	rep = opendir(nameDossier);
 	if(rep == NULL){
 		perror("Problème lors de l'ouverture du fichier");
 	}
+
 	struct dirent* fichierLu = NULL; 
 	//maintenant qu'on a ouvert le répertoire, il faut créer des fichier
 	//de la même taille
-	
 	//ex pour 10 100 1000 10 000 100 000 fichiers
 	int N;
-	char fichier[255];
-	char name[255]; //= strcat(cheminDuDossier, "fichier");
-	sprintf((char *) &name, "%s%s",cheminDuDossier,"/fichier");
+	char nameFichierFinal[255];
+	char namePrevFichier[255];
+	sprintf((char *) &namePrevFichier, "%s%s",nameDossier,"/fichier");
 	FILE *f;
 	int i;
 	// -----------------------------------------------------------------------------------------
 
 	printf("On commence avec les fichiers vides \n");
 
-	for(N=10;N<=nombreDeFichierMaxCree;N=10*N){
+	for(N=10;N<=nombreDeFichierMaxCree;N=5*N)
+	{
 		
-
 		// On crée tous les fichiers dans un dossier
 		for(i=0;i<N;i++){
 
-			sprintf((char *) &fichier, "%s%i.txt",name,i);
+			sprintf((char *) &nameFichierFinal, "%s%i.txt",namePrevFichier,i);
 		
-			f = fopen(fichier,"w");
+			// On n'utilise pas la fonction de 'copy.h' parce qu'elle remplit les fichiers alors qu'on ne le veut pas forcément
+			create_file(nameFichierFinal, 0);
+			/*
+			f = fopen(nameFichierFinal,"w");
 			if (f == NULL){
 				perror("problème lors de l'ouverture du fichier");
 			}
@@ -83,11 +92,13 @@ int main(int argc, char *argv[]){
 				if(fclose(f))
 					perror("problème lors de la fermeture d'un fichier");
 			}
+			*/
 
 		}
 		// Une fois qu'ils sont créés, on rebobine
 		rewinddir(rep);
 		start_timer(t);
+
 		// On lit le dossier pour voir combien de temps ça prend
 		while((fichierLu = readdir(rep))!=NULL)
 		{
@@ -96,53 +107,60 @@ int main(int argc, char *argv[]){
 		//j'utilise le record_n parce qu'il faut diviser par le nombre d'de fichier parce que qui est
 		//interessant c'est le temps par fichier
 		write_record_n(record,N,stop_timer(t),N);
-		//write_record(recordTot, N, stop_timer(t));
+
+		//supression de tous les fichiers:
+
+		for(i=0;i<N;i++){
+
+			sprintf((char *) &nameFichierFinal, "%s%i.txt",namePrevFichier,i);
+			
+			// On supprime les fichiers, avec la fonction fournie dans le 'copy.h' -> n'arrive pas à l'importer...
+			 rm(nameFichierFinal); 	
+			//if(remove(nameFichierFinal));
+		}
+
 		printf("test fini pour N=%d\n",N);
 	}
 
-	//supression de tous les fichiers:
-
-	for(i=0;i<N;i++){
-
-		sprintf((char *) &fichier, "%s%i.txt",name,i);
-		
-		if(remove(fichier));
-	}
-
+	
 	printf("Fini les fichiers vides \n");
 
 	// ----------------------------------------------------------------------------------------------------------
 
 	printf("On commence avec les fichiers remplis \n");
+	char buf[longFichierAlloue];
 
-	//timer *t2 = timer_alloc();
-	
-	char buf[longueurChaineGene]; // Chaine à placer dans le fichier
-
-	for(N=10;N<=nombreDeFichierMaxCree;N=10*N){
+	for(N=10;N<=nombreDeFichierMaxCree;N=5*N){
 		int i;
 
-		for(i=0;i<N;i++){
-
-			sprintf((char *) &fichier, "%s%i.txt",name,i);
+		for(i=0;i<N;i++)
+		{
+			sprintf((char *) &nameFichierFinal, "%s%i.txt",namePrevFichier,i);
 		
-			f = fopen(fichier,"w");
-			if (f == NULL){
-				perror("problème lors de l'ouverture du fichier");
+			// On crée un fichier avec la méthode de copy.c qui le remplit directement -> n'arrive pas à l'importer...
+			//create_file(nameFichierFinal, longFichierAlloue);
+
+			 // Méthode de réserve
+			f = fopen(nameFichierFinal, "w");
+			if (f == NULL)
+			{
+				printf("Le fichier %i ne peut pas être créé \n", i);
 			}
 			
-			rand_str(buf, longueurChaineGene);
-			
+			chaineCaractere(buf, longFichierAlloue);
 			fprintf(f, "%s", buf);
 
-			if(fclose(f))
-			{
-				perror("problème lors de la fermeture d'un fichier");
+			if(fclose(f)){
+					perror("problème lors de la fermeture d'un fichier");
 			}
-
+			
 		}
+
 		rewinddir(rep);
+
+		// On réutilise le pointeur déjà alloué
 		start_timer(t);
+
 		while((fichierLu = readdir(rep))!=NULL)
 		{
 			//printf("Le fichier lu s'appelle '%s'\n", fichierLu->d_name);
@@ -150,28 +168,34 @@ int main(int argc, char *argv[]){
 		//j'utilise le record_n parce qu'il faut diviser par le nombre d'de fichier parce que qui est
 		//interessant c'est le temps par fichier
 		write_record_n(record2,N,stop_timer(t),N);
-		printf("test fini pour N=%d\n",N);
-	}
-	//supression de tous les fichiers:
 
-	for(i=0;i<N;i++){
+		//supression de tous les fichiers:
+		for(i=0;i<N;i++)
+		{
+			sprintf((char *) &nameFichierFinal, "%s%i.txt",namePrevFichier,i);
+			
+			// On supprime le fichier avec la méthode de copy.c -> ne fonctionne pas, n'arrive pas à l'importer...
+			rm(nameFichierFinal);
 
-		sprintf((char *) &fichier, "%s%i.txt",name,i);
-		
-		if(remove(fichier));
+			//if (remove(nameFichierFinal));
+		}
+
+		printf("test full fini pour N=%d\n",N);
 	}
 
 	if(closedir(rep) == -1)
 	{
 		return -1; // EXIT_FAILURE
 	}
+
 	//suppression du répertoire (vide):
-	if(rmdir("./repertoire")){
+	if(rmdir(nameDossier))
+	{
 		perror("rmdir");
 	}
 
 	recorder_free(record);
-	//recorder_free(recordTot);
+	recorder_free(record2);
 	timer_free(t);
 
 	printf("Le programme a fini... \n");
